f299daa897ea6d6551d9d6f4370cd63abe895e24
aries0d0f <aries0d0f@gmail.com>
2017-04-29 23:51:02 +0800

    Add blog link

--- assets/config.rb
+++ assets/config.rb
@@ -11,7 +11,7 @@ javascripts_dir = "js"
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 
-output_style = :nested
+output_style = :compressed
 asset_cache_buster = :none
 cache = false
 sourcemap = true
